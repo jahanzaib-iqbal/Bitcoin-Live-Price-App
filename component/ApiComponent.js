eb08ca2f06e7d158667a98ce4813ad7469f5d4e0
Jahanzaib Iqbal <72562056+jahanzaib-iqbal@users.noreply.github.com>
2023-10-24 19:40:00 +0500

    Update ApiComponent.js

--- component/ApiComponent.js
+++ component/ApiComponent.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { Text, View, FlatList, StyleSheet } from "react-native";
+import React, { useEffect, useState } from 'react';
+
+import {Text, View, FlatList, StyleSheet, ActivityIndicator } from "react-native";
 import CustomApiHook from "./CustomApiHook";
 import * as Animatable from "react-native-animatable";
 
@@ -7,30 +8,43 @@ const ApiComponent = () => {
   const { bitcoinData } = CustomApiHook(
     "https://api.coindesk.com/v1/bpi/currentprice.json"
   );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false); // Set loading to false when data is loaded
+    }, 1000); // Simulated 1-second delay
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Bitcoin Live Price Today</Text>
       </View>
-      <View style={styles.listContainer}>
-        <FlatList
-          data={Object.entries(bitcoinData)}
-          keyExtractor={(item) => item[0]}
-          renderItem={({ item }) => (
-            <Animatable.View
-              animation="fadeInUpBig"
-              duration={1000}
-              style={styles.priceItem}
-            >
-              <Text style={styles.itemHeading}>
-                {item[0]}: {item[1].rate}
-              </Text>
-              <Text style={styles.itemText}>{item[1].description}</Text>
-            </Animatable.View>
-          )}
-        />
-      </View>
+
+      {loading ? (
+        // Display a loading indicator while data is loading
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <View style={styles.listContainer}>
+          <FlatList
+            data={Object.entries(bitcoinData)}
+            keyExtractor={(item) => item[0]}
+            renderItem={({ item }) => (
+              <Animatable.View
+                animation="fadeInUpBig"
+                duration={1000}
+                style={styles.priceItem}
+              >
+                <Text style={styles.itemHeading}>
+                  {item[0]}: {item[1].rate}
+                </Text>
+                <Text style={styles.itemText}>{item[1].description}</Text>
+              </Animatable.View>
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 };
